6bf5ed0dae31d12996397ccd6a3128eb2fe610a8
joniibra <joniibra@users.noreply.github.com>
2017-09-20 10:34:39 +0700

    Delete cleanup.php

--- countshare/cleanup.php
+++
@@ -1,5 +0,0 @@
-<?php
-require("share_count.php");
-$count = new shareCount;
-$count->cleanCache(isset($_REQUEST['kill']));
-echo 'Cache has been ' . (isset($_REQUEST['kill']) ? 'deleted' : 'cleaned') . '.';
